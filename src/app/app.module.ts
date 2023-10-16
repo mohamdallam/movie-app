23a685b5c736240e73ead23d0900df96624c6c33
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-16 16:08:28 +0300

    creat  movie app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieAppComponent } from './movie-app/movie-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-app/movie-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-app',
+  templateUrl: './movie-app.component.html',
+  styleUrls: ['./movie-app.component.css']
+})
+export class MovieAppComponent {
+
+}
