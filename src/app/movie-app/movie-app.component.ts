61479930bcd91727ca99ae7d824321aa926b3f88
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-16 18:40:05 +0300

    defined the type of data in interface and call Api

--- src/app/movie-app/movie-app.component.ts
+++ src/app/movie-app/movie-app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { MovieApiService } from '../services/movie-api.service';
+import { Movie } from '../interfaces/movie';
 @Component({
   selector: 'app-movie-app',
   templateUrl: './movie-app.component.html',
   styleUrls: ['./movie-app.component.css']
 })
 export class MovieAppComponent {
+  AllMovies!: Array<Movie>;
 
   constructor (private movieService: MovieApiService ){}
   ngOnInit() {
-    this.movieService.getMovieList().subscribe(( data )=>console.log(data)
+    this.movieService.getMovieList().subscribe(( data:any )=> this.AllMovies = data.results
      )
   }
    
