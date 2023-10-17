5b20eb75559c09f693065f60b3bed3698ddbdd09
mohamdallam <mohamedahmedallam1@gmail.com>
2023-10-17 15:12:29 +0300

    create-interface-recommendation&update-details

--- src/app/movie-app/movie-app.component.ts
+++ src/app/movie-app/movie-app.component.ts
@@ -6,21 +6,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-movie-app',
   templateUrl: './movie-app.component.html',
-  styleUrls: ['./movie-app.component.css']
+  styleUrls: ['./movie-app.component.css'],
 })
 export class MovieAppComponent {
   AllMovies!: Array<Movie>;
 
-  constructor (private movieService: MovieApiService , private router: Router ){}
+  constructor(private movieService: MovieApiService, private router: Router) {}
   ngOnInit() {
-    this.movieService.getMovieList().subscribe(( data:any )=> this.AllMovies = data.results
-     )
-  }
-   
-  redirectToDetails(id : any){
-
-    this.router.navigate(['Product-details',id])
+    this.movieService
+      .getMovieList()
+      .subscribe((data: any) => (this.AllMovies = data.results));
   }
 
-
+  redirectToDetails(id: any) {
+    this.router.navigate(['Product-details', id]);
+  }
 }
