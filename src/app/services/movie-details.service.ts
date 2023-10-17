55c6ee48d3975880fd146e92cb068f59e4c6fa67
mohamdallam <mohamedahmedallam1@gmail.com>
2023-10-17 14:04:22 +0300

    movie-recommendation

--- src/app/services/movie-details.service.ts
+++ src/app/services/movie-details.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieDetailsService {
-
   constructor(private http: HttpClient) {}
-  getMovieDetails(id :string) {
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=391d402928dd26be93f24ece2b7bdeaa`)
-        }}
+  getMovieDetails(id: string) {
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=391d402928dd26be93f24ece2b7bdeaa`
+    );
+  }
+}
